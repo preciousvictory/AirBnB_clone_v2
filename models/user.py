fa7f8859101fb1b449367ebab6b3578ae11daf48
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-22 12:41:29 -0700

    AirBnB clone

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 import os
 
 
@@ -13,6 +14,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user", cascade="all, delete")
     else:
         email = ''
         password = ''
