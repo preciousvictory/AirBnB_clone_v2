a033adbbd8178f5b4cdcaa7753b48c94a9f6dd28
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-22 17:33:05 -0700

    AirBnB clone

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+import os
 
 class Amenity(BaseModel):
-    name = ""
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity, viewonly=False)
+    else:
+        name = ""
--- models/place.py
+++ models/place.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 from models.review import Review
 import os
+
+
+if os.getenv('HBNB_TYPE_STORAGE') == "db":
+    place_amenity = Table('place_amenity', Base.metadata,
+                        Column('place_id', String(60),
+                               ForeignKey('places.id'), primary_key=True,
+                               nullable=False),
+                        Column('amenity_id', String(60), 
+                               ForeignKey('amenities.id'), primary_key=True,
+                               nullable=False)
+    )
 
 
 class Place(BaseModel, Base):
@@ -22,6 +33,8 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", backref="place", cascade="all, delete")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 backref="place" , viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -39,9 +52,27 @@ class Place(BaseModel, Base):
         def reviews(self):
             """ returns the list of Review instances with place_id equals to
             the current Place.id"""
-            all_rev = storage.all(Review)
+            all_reviews = storage.all(Review)
             list_rev = []
-            for value in all_rev.values():
+            for value in all_reviews.values():
                 if review.place_id == Place.id:
                     list_rev.append(value)
             return list_rev
+        @property
+        def amenities(self):
+            """returns the list of Amenity instances based on the attribute
+            amenity_ids that contains all Amenity.id linked to the Place"""
+            all_ameniies = storage.all(Amenity)
+            list_ame = []
+            for value in all_amenities.values():
+                if value.id in self.amenity_ids:
+                    list_ame.append(value)
+            return list_ame
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter attribute amenities that handles append method foradding
+            an Amenity.id to the attribute amenity_ids. """
+            from models.amenity import Amenity
+            if isinstance(obj, Amenity) and type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
