6a13f46f152f2c81f058dc94f2db8aafb52b8fb5
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-06 14:42:19 -0700

    webstatics

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,8 +15,8 @@ echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf '/data/web_static/releases/test/' '/data/web_static/current'
 
-sudo chown -fR ubuntu:ubuntu /data/ 
+sudo chown -fR ubuntu:ubuntu /data 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+#!/usr/bin/env bash
 """
 a Fabric script that generates a .tgz archive from the contents of the
 web_static folder of your AirBnB Clone repo, using the function do_pack.
 """
-
 
 from fabric.api import local
 from datetime import datetime
@@ -16,7 +16,7 @@ def do_pack():
             datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
 
     local("makdir -p versions")
-    r = local(f"tar -cvf {path} web_static")
+    r = local(f"tar -cvzf {path} web_static")
 
     if r.failed:
         return path
