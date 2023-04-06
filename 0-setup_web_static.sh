e52df38756ffe9a8007bedb00e57bb3b65ea1497
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-06 12:40:05 -0700

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,43 +1,23 @@
 #!/usr/bin/env bash
 # a Bash script that sets up your web servers for the deployment of web_static.
 # Install Nginx if it not already installed
-if ! [ -d $n ]
-then
-        sudo apt-get -y update
-        sudo apt-get -y install nginx
-        echo 'Hello World!' > /var/www/html/index.html
-        service nginx start
-fi
+sudo apt-get -y update
+sudo apt-get -y install nginx
+echo 'Hello World!' > /var/www/html/index.html
+mkdir -p /data/web_static/releases/test/
 
-# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
-DIR=/data/web_static/releases/test/
-if ! [ -d $DIR ]
-then
-        mkdir -p /data/web_static/releases/test/
-fi
+touch /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
-# Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
-FILE=/data/web_static/releases/test/
-if ! [ -f $FILE ]
-then
-        touch /data/web_static/releases/test/index.html
-        echo "<html>
-        <head>
-        </head>
-        <body>
-                AirBnB Clone!
-        </body>
-        </html>" > /data/web_static/releases/test/index.html
-fi
-
-# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
 touch /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -fR ubuntu:ubuntu /data/
-
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). 
+sudo chown -fR ubuntu:ubuntu /data/ 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-
 sudo service nginx restart
