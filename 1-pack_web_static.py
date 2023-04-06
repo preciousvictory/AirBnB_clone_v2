ff51ba67483dd2bf45d79488f484cdb050dd35e5
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-06 14:33:14 -0700

    web statics

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,18 +3,21 @@
 a Fabric script that generates a .tgz archive from the contents of the
 web_static folder of your AirBnB Clone repo, using the function do_pack.
 """
+
+
 from fabric.api import local
-import datetime
-import os
+from datetime import datetime
+
 
 def do_pack():
     """function do_pack!"""
-    a = datetime.datetime.now()
 
-    path = f"versions/web_static_{a.year}{a.month}{a.day}{a.hour}{a.minute}{a.second}.tgz"
+    path = f"versions/web_static_{}.tgz".format(
+            datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
+
     local("makdir -p versions")
-    local(f"tar -cvf {path} web_static")
+    r = local(f"tar -cvf {path} web_static")
 
-    if os.path.exists(path):
+    if r.failed:
         return path
     return None
