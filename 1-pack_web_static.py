4ad8e9e5175a1bf3c7aa8798253a6a50ab87994b
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-09 10:13:26 -0700

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python3
 #!/usr/bin/env bash
-"""
-a Fabric script that generates a .tgz archive from the contents of the
-web_static folder of your AirBnB Clone repo, using the function do_pack.
-"""
+#!/usr/bin/python3
+'''a Fabric script that generates a .tgz archive'''
 
 from fabric.api import local
 from datetime import datetime
