302b2e18c12298881c5fe5b0b4353395818364d3
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-09 10:16:41 -0700

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
 #!/usr/bin/python3
-'''a Fabric script that generates a .tgz archive'''
+
+"""a Fabric script that generates a .tgz archive"""
 
 from fabric.api import local
 from datetime import datetime
