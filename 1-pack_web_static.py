ef162275c73e32efd079b562b92babd3610dd6e9
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-09 10:21:34 -0700

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,6 @@ from datetime import datetime
 
 def do_pack():
     """function do_pack!"""
-
     path = f"versions/web_static_{}.tgz".format(
             datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
 
