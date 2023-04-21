1bdc632d888d5ef966df0f6f0695638ec818a063
preciousvictory <victoryexcel2018@gmail.com>
2023-04-21 15:03:26 -0700

    AirBnB clone

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,29 +4,30 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     """hello"""
     return ('Hello HBNB!')
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """"HBNB"""
     return ('HBNB')
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_text(text):
     """display 'C ', followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python', defaults={'text': 'ís cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
     """display 'Python ', followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
