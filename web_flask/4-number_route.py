f8d15315b991378af6453397bf1b03e55a8ebc33
preciousvictory <victoryexcel2018@gmail.com>
2023-04-21 15:55:02 -0700

    AirBnB clone

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """hello"""
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb')
+def hbnb():
+    """"HBNB"""
+    return ('HBNB')
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """display 'C ', followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """display 'Python ', followed by the value of the text variable"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def num(n):
+    """display 'n is a number' only if n is an integer"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
